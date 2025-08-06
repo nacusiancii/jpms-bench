7f49714aa9a8249283b3de4a967d06d3cab7b8b3
murali <murali.krishna@nextory.com>
2025-08-06 22:10:00 +0530

    Initial commit

---
+++ src/test/java/com/jpms/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpms.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
